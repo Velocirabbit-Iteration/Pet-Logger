27038a025a1a8c19900df0ae60afcb88a0326a3b
adeebbayat <adeebbayat@yahoo.com>
2024-03-13 16:22:18 -0700

    removed href from setting and ?

--- src/components/NavComponent.jsx
+++ src/components/NavComponent.jsx
@@ -18,10 +18,10 @@ const NavComponent = () => {
           {/* <img src='https://media.istockphoto.com/id/1386479313/photo/happy-millennial-afro-american-business-woman-posing-isolated-on-white.jpg?s=612x612&w=0&k=20&c=8ssXDNTp1XAPan8Bg6mJRwG7EXHshFO5o0v9SIj96nY='></img> */}
           <span>User Name</span>
         </div>
-        <a href='#'>
+        <a>
           <SettingIcon />
         </a>
-        <a href='#'>
+        <a>
           <HelpIcon />
         </a>
       </nav>
