33eacf82339c444946cd63031f4eabb495b63754
Marselena Sequoia <github.seaplanes@simplelogin.co>
2024-03-13 11:02:27 -0400

    Add cors to login/signup request
    Co-authored-by: Emma Ijiogbe <emmagawd@users.noreply.github.com>
    Co-authored-by: tenn501 <tenn501@users.noreply.github.com>
    Co-authored-by: Adeeb Bayat <adeebbayat@yahoo.com>

--- src/components/LoginComponent.jsx
+++ src/components/LoginComponent.jsx
@@ -1,47 +1,47 @@
-import React from "react";
-
-
-
-// call to http://localhost:3000/auth/login
-// call to http://localhost:3000/auth/signup
-
+import React from 'react';
+import cors from 'cors';
 
 const LoginComponent = () => {
-  
   function handleLogin(event) {
     event.preventDefault();
     const userName = document.querySelector('#usernameInput').value;
     const password = document.querySelector('#passwordInput').value;
-    
-    fetch('http://localhost:3000/auth/login',{
+
+    fetch('http://localhost:3000/auth/login', {
       method: 'POST',
-      body: JSON.stringify({userName,password})
+      body: JSON.stringify({ userName, password }),
+      mode: 'cors',
     });
   }
-  
+
   function handleSignUp(event) {
     event.preventDefault();
     const userName = document.querySelector('#usernameInput').value;
     const password = document.querySelector('#passwordInput').value;
-    
-    fetch('http://localhost:3000/auth/signup',{
+
+    fetch('http://localhost:3000/auth/signup', {
       method: 'POST',
-      body: JSON.stringify({userName,password})
+      body: JSON.stringify({ userName, password }),
+      mode: 'cors',
     });
   }
-  
-  return(
+
+  return (
     <>
       <h1>Login / Sign Up</h1>
 
       <form>
-        <input type="text" id='usernameInput' placeholder="username"/>
-        <input type="password" id='passwordInput' placeholder="password"/>
-        <button type='button' onClick={(e) => handleLogin(e)}>Log In</button>
-        <button type='button' onClick={(e) => handleSignUp(e)}>Sign Up</button>
+        <input type='text' id='usernameInput' placeholder='username' />
+        <input type='password' id='passwordInput' placeholder='password' />
+        <button type='button' onClick={(e) => handleLogin(e)}>
+          Log In
+        </button>
+        <button type='button' onClick={(e) => handleSignUp(e)}>
+          Sign Up
+        </button>
       </form>
     </>
-  )
-}
+  );
+};
 
 export default LoginComponent;
