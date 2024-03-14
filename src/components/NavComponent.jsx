fbe11661a4feaf75285ddf54bdc3e11e91216c27
Marselena Sequoia <github.seaplanes@simplelogin.co>
2024-03-14 11:11:57 -0400

    Display user name on login
    Co-authored-by: Emma Ijiogbe <emmagawd@users.noreply.github.com>

--- src/components/NavComponent.jsx
+++ src/components/NavComponent.jsx
@@ -4,10 +4,10 @@ import SettingIcon from './SettingIcon';
 import HelpIcon from './HelpIcon';
 import DogIcon from './DogIcon';
 
-const NavComponent = () => {
-  const nameArray = ['Adeeb','Lillian','Howard','Emma','Marselena']
-  const selectedName = nameArray[Math.round(Math.random() * 5)];
-  
+const NavComponent = ({ currentUserId }) => {
+  // const nameArray = ['Adeeb', 'Lillian', 'Howard', 'Emma', 'Marselena'];
+  // const selectedName = nameArray[Math.round(Math.random() * 5)];
+
   return (
     <div className='navheader'>
       <a className='logo' href='/#/user'>
@@ -19,11 +19,9 @@ const NavComponent = () => {
         <div className='profileinfocontainer'>
           <img src='https://as1.ftcdn.net/v2/jpg/02/56/63/58/1000_F_256635898_A8FkyVvBHXOHem9viOKnEWx7ZRiWB2mB.jpg'></img>
           {/* <img src='https://media.istockphoto.com/id/1386479313/photo/happy-millennial-afro-american-business-woman-posing-isolated-on-white.jpg?s=612x612&w=0&k=20&c=8ssXDNTp1XAPan8Bg6mJRwG7EXHshFO5o0v9SIj96nY='></img> */}
-          <span>{selectedName}</span>
+          <span>{sessionStorage.getItem('name') || null}</span>
         </div>
-        <a href='/'>
-          Login
-        </a>
+        <a href='/'>Login</a>
         <a>
           <HelpIcon />
         </a>
